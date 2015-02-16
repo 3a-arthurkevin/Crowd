360f0a084a2d8fd36b97275dc81264f750cb27ec
Pierr0 <p_bloquet@yahoo.fr>
2015-02-16 19:31:10 +0100

    Asset v2
    Asset avancé, animation scale "corect" debut de scripting

---
+++ Crowd/CrowdControl/Assets/Script/ArmyScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ArmyScript : MonoBehaviour {
+
+	[SerializeField]
+	private List<SoldierScript>_army_Soldiers;
+
+	[SerializeField]
+	private List<KnightScript>_army_Knight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Crowd/CrowdControl/Assets/Script/GameManagerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManagerScript : MonoBehaviour {
+
+	[SerializeField]
+	private ArmyScript _army1;
+
+	[SerializeField]
+	private ArmyScript _army2;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
